cdbf68bfca41a0d1011e7ce6285ad3a9203436cb
Milica Vucic <vucic.milica99@gmail.com>
2023-08-19 12:49:08 +0200

    updated loop inversion

--- LoopInversion/LoopInversion.cpp
+++ LoopInversion/LoopInversion.cpp
@@ -63,8 +63,12 @@ struct LoopInversionPass : public LoopPass {
       if (isa<ICmpInst>(&Instr)) {
         LoopCounter = VariablesMap[Instr.getOperand(0)];
 
-        // Ako se granica cuva u promenljivoj, onda: VariablesMap[Instr.getOperand(1)];
-        LoopBound = Instr.getOperand(1);
+        // Potrebno je proveriti da li je granica konstanta ili je sacuvana u nekoj pomocnoj promenljivoj
+        if (ConstantInt *ConstInt = dyn_cast<ConstantInt>(Instr.getOperand(1))) {
+          LoopBound = Instr.getOperand(1);
+        } else {
+          LoopBound = VariablesMap[Instr.getOperand(1)];
+        }
       }
     }
 
@@ -94,41 +98,77 @@ struct LoopInversionPass : public LoopPass {
     std::unordered_map<Value *, Value *> Mapping = {};
 
     for (Instruction &Instr : *From) {
-      InstrCopy = Instr.clone();
-      InstrCopy->insertBefore(To->getTerminator());
-      Mapping[&Instr] = InstrCopy;
+      if (!Instr.isTerminator()) {
+        InstrCopy = Instr.clone();
+        InstrCopy->insertBefore(To->getTerminator());
+        Mapping[&Instr] = InstrCopy;
 
-      for (size_t i = 0; i < InstrCopy->getNumOperands(); ++i) {
-        if (Mapping.find(InstrCopy->getOperand(i)) != Mapping.end())
-          InstrCopy->setOperand(i, Mapping[Instr.getOperand(i)]);
+        for (size_t i = 0; i < InstrCopy->getNumOperands(); ++i) {
+          if (Mapping.find(InstrCopy->getOperand(i)) != Mapping.end())
+            InstrCopy->setOperand(i, Mapping[Instr.getOperand(i)]);
+        }
       }
     }
 
     To->getTerminator()->setSuccessor(0, LoopBasicBlocks.front());
   }
 
+//  BasicBlock *CreateBasicBlock()
+//  {
+//    BasicBlock *NewBasicBlock = BasicBlock::Create(LoopBasicBlocks.front()->getContext());
+//    NewBasicBlock->insertInto(LoopBasicBlocks.front()->getParent(), LoopBasicBlocks.front());
+//
+//    Instruction *InstrCopy = nullptr;
+//    std::unordered_map<Value *, Value *> Mapping = {};
+//
+//    IRBuilder<> Builder(NewBasicBlock);
+//    for (Instruction &Instr : *LoopBasicBlocks.front()) {
+//      InstrCopy = Instr.clone();
+//      Builder.Insert(InstrCopy);
+//      Mapping[&Instr] = InstrCopy;
+//
+//      for (size_t i = 0; i < InstrCopy->getNumOperands(); ++i) {
+//        if (Mapping.find(InstrCopy->getOperand(i)) != Mapping.end())
+//          InstrCopy->setOperand(i, Mapping[Instr.getOperand(i)]);
+//      }
+//    }
+//
+//    errs() << "--- Copied basic block ---\n";
+//    for (Instruction &Instr : *NewBasicBlock) {
+//      Instr.print(errs(), false);
+//      errs() << "\n";
+//    }
+//
+//    return NewBasicBlock;
+//  }
+
   void LoopInversion(Loop *L)
   {
+    // BasicBlock *NewBasicBlock = CreateBasicBlock();
+    // Alternativa za kopiranje BB jeste da se sve instrukcije kreiraju rucno
     BasicBlock *NewBasicBlock = BasicBlock::Create(LoopBasicBlocks.front()->getContext());
-    // ubacuje se ispred prvog BB koji se odnosi na petlju
-    NewBasicBlock->insertInto(LoopBasicBlocks.front()->getParent(),
-                              LoopBasicBlocks.front()->getTerminator()->getSuccessor(0));
+    NewBasicBlock->insertInto(LoopBasicBlocks.front()->getParent(), LoopBasicBlocks.front());
 
     IRBuilder<> Builder(NewBasicBlock);
-//    %4 = load i32, ptr %2, align 4
-//    %5 = icmp slt i32 %4, 10
-//    br i1 %5, label %6, label %10
+    // %4 = load i32, ptr %2, align 4
+    // %5 = icmp slt i32 %4, 10
+    // br i1 %5, label %6, label %10
     Value *Load = Builder.CreateLoad(Type::getInt32Ty(NewBasicBlock->getContext()), LoopCounter);
-    Value *CMP = Builder.CreateICmp(CmpInst::ICMP_SLT, Load,
-                                    ConstantInt::get(Type::getInt32Ty(NewBasicBlock->getContext()), BoundValue));
+    Value *CMP = Builder.CreateICmp(CmpInst::ICMP_SLT, Load, LoopBound);
     Builder.CreateCondBr(CMP,
                          LoopBasicBlocks.front()->getTerminator()->getSuccessor(0),
                          LoopBasicBlocks.front()->getTerminator()->getSuccessor(1));
 
+    errs() << "--- Instructions ---\n";
+    for (Instruction &Instr : *NewBasicBlock) {
+      Instr.print(errs(), false);
+      errs() << "\n";
+    }
+
     L->getLoopPreheader()->getTerminator()->setSuccessor(0, NewBasicBlock);
     LoopBasicBlocks.front()->moveBefore(LoopBasicBlocks.back());
     CopyInstructions(LoopBasicBlocks.back(), LoopBasicBlocks[LoopBasicBlocks.size() - 2]);
-    LoopBasicBlocks.back()->eraseFromParent();
+//    LoopBasicBlocks.back()->eraseFromParent(); <- ovde puca
   }
 
   bool runOnLoop(Loop *L, LPPassManager &) override {
